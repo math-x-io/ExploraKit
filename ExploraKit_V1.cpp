4a74dcc5c7ac4b75043fc258d1fb4fa25571f215
math-x <97481289+math-x-io@users.noreply.github.com>
2023-08-12 17:12:27 +0200

    Update ExploraKit_V1.cpp
    add comments

--- ExploraKit_V1.cpp
+++ ExploraKit_V1.cpp
@@ -4,8 +4,9 @@
 #include <filesystem>
 #include <chrono>
 
-namespace fs = std::filesystem;
+namespace fs = std::filesystem;  // Namespace alias for the filesystem library
 
+// Check if a directory is special
 bool isSpecialDirectory(const fs::path &directory)
 {
     static const std::vector<std::string> specialDirs = {"$Recycle.Bin", "$Windows.~BT", "$Windows.~WS"};
@@ -13,12 +14,13 @@ bool isSpecialDirectory(const fs::path &directory)
     {
         if (directory.filename() == specialDir)
         {
-            return true;
+            return true;  // Directory is special
         }
     }
-    return false;
+    return false;  // Directory is not special
 }
 
+// Update the progress bar
 void updateProgressBar(uintmax_t currentSize, uintmax_t totalSize)
 {
     float progress = static_cast<float>(currentSize) / totalSize;
@@ -38,9 +40,10 @@ void updateProgressBar(uintmax_t currentSize, uintmax_t totalSize)
         }
     }
     std::cout << "] " << int(progress * 100.0) << "%";
-    std::cout.flush();
+    std::cout.flush();  // Flush the output buffer for immediate display
 }
 
+// Recursive search for a file in a directory
 bool searchFileRecursive(const fs::path &directory, const std::string &targetFileName, uintmax_t totalSize, uintmax_t &currentSize)
 {
     try
@@ -49,7 +52,7 @@ bool searchFileRecursive(const fs::path &directory, const std::string &targetFil
         {
             if (isSpecialDirectory(entry.path()))
             {
-                continue;
+                continue;  // Skip special directories
             }
 
             try
@@ -59,7 +62,7 @@ bool searchFileRecursive(const fs::path &directory, const std::string &targetFil
                     uintmax_t subDirTotalSize = fs::space(entry).capacity;
                     if (searchFileRecursive(entry, targetFileName, subDirTotalSize, currentSize))
                     {
-                        return true;
+                        return true;  // File found in subdirectory
                     }
                 }
                 else if (fs::is_regular_file(entry))
@@ -68,23 +71,24 @@ bool searchFileRecursive(const fs::path &directory, const std::string &targetFil
                     if (fileName == targetFileName)
                     {
                         std::cout << std::endl;
-                        std::cout << "File found : " << entry.path() << std::endl;
-                        return true;
+                        std::cout << "File found: " << entry.path() << std::endl;
+                        return true;  // File found in the current directory
                     }
                 }
 
                 currentSize += entry.file_size();
-                updateProgressBar(currentSize, totalSize);
+                updateProgressBar(currentSize, totalSize); 
             }
             catch (const std::exception &ex)
             {
+                // Handle exceptions while accessing directories
             }
         }
-        return false;
+        return false;  // File not found in the directory
     }
     catch (const std::exception &ex)
     {
-        return false;
+        return false;  // Handle exceptions while accessing the directory
     }
 }
 
@@ -103,8 +107,10 @@ int main()
 
     std::vector<fs::path> storageList;
 
+    // Choose to search on all drives or specific drives
     if (exploreAll == 'y' || exploreAll == 'Y')
     {
+        // Search on all available drives
         for (char driveLetter = 'A'; driveLetter <= 'Z'; ++driveLetter)
         {
             fs::path drivePath(std::string(1, driveLetter) + ":/");
@@ -116,6 +122,7 @@ int main()
     }
     else
     {
+        // Search on user-specified drives
         for (char driveLetter = 'A'; driveLetter <= 'Z'; ++driveLetter)
         {
             fs::path drivePath(std::string(1, driveLetter) + ":/");
@@ -126,6 +133,7 @@ int main()
             }
         }
 
+        // Ask the user to enter a valid drive letter
         std::string selectedDrive;
         bool validDrive = false;
         do
@@ -148,12 +156,13 @@ int main()
         storageList.push_back(fs::path(selectedDrive + ":/"));
     }
 
+    // Record the start time of the search
     auto start = std::chrono::high_resolution_clock::now();
 
     bool found = false;
     for (const fs::path &storage : storageList)
     {
-        std::cout << "Search in : " << storage << std::endl;
+        std::cout << "Search in: " << storage << std::endl;
         if (fs::is_directory(storage))
         {
             uintmax_t currentSize = 0;
@@ -168,15 +177,16 @@ int main()
         }
     }
 
+    // Record the end time of the search
     auto end = std::chrono::high_resolution_clock::now();
 
+    // Display the search result and duration
     if (!found)
     {
         std::cout << "The file '" << targetFileName << "' was not found." << std::endl;
     }
 
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    std::cout << "\nSearch duration : " << duration << " ms" << std::endl;
+    std::cout << "\nSearch duration: " << duration << " ms" << std::endl;
 
     return 0;
-}
